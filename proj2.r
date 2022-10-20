6bb87439cfb6afc7b329d68ccc2830cb8f51c85d
Morgan <m.gallagher-14@sms.ed.ac.uk>
2022-10-20 18:15:07 +0100

    adding descriptions

--- proj2.r
+++ proj2.r
@@ -9,6 +9,41 @@
       #example code for each strategy n=5 and n=50, wrote remarks on results 
 # Banan - created dloop function and ran the example and plot
 
+
+#Objective: 
+#This code intends to simulate the prisoner problem to determine which strategy
+#is the best for all prisoners to be released. 
+#The prisoner problem is set up as follows, there are 2n prisoners that have a 
+#unique number from 1 to 2n. The prison has a room that contains 2n boxes. Each 
+#box contains a random card that has a unique number, 1 to 2n. The prisoners' 
+#objective is to find the card with their number on it by opening a maximum of 
+#n boxes. After one prisoner's turn the room is returned to its original state. 
+#If all prisoners succeed in finding their number, all prisoners will be 
+#released. We assume that the prisoners can implement 1 of 3 strategies. 
+
+  #Strategy one is that the prisoner starts with the box that has 
+  #their number on it. They open the box and read what number is
+  # on the card. If the number on the card is not their number they
+  #proceed to the box with the number on the card until they find 
+  #their card number
+
+  #Strategy two is the same procedure as strategy one, but the prisoner
+  #starts at a random box instead of the box with their prisoner number
+
+  #Strategy three is the prisoner opens n random boxes and checks if the
+  #cards inside matches their prisoner number
+
+#To simulate all prisoners being released we first create 3 functions that 
+#return if one prisoner is successful in finding their number while following 
+#each of the three strategies. We then create function Pone which tells us the 
+#probability of one prisoner being successful. We then construct Pall to tell us 
+#the probability that all prisoners find their number. To further support our 
+#results from Pall to determine which strategy is best, we construct function 
+#dloop to examine the probability of loops smaller than n existing in a random 
+#shuffling of the cards in the boxes. We visualize this using a histogram. 
+
+
+  
 strategy_one <- function(n, k, card){
 
 	# Calculate the probability (0 or 1) a prisoner finds their number 
@@ -32,17 +67,17 @@ strategy_one <- function(n, k, card){
 
 strategy_two <- function(n, k, card){
 
-        # Calculate the probability (0 or 1) a prisoner finds their number
-        # in 2n boxes given n tries, starting with a random box number.
-	      # If a card contains a number of a box previously opened, it is failure 
+  # Calculate the probability (0 or 1) a prisoner finds their number
+  # in 2n boxes given n tries, starting with a random box number.
+	# If a card contains a number of a box previously opened, it is failure 
 
-        # Input:
-        # n - number of tries/max boxes prisoner can open
-        # k - prisoner's number
-        # card - vector of unique random generated numbers of length 2n
+  # Input:
+  # n - number of tries/max boxes prisoner can open
+  # k - prisoner's number
+  # card - vector of unique random generated numbers of length 2n
 
-        # Output:
-        # success - 1 if the prisoner finds the number, 0 otherwise
+  # Output:
+  # success - 1 if the prisoner finds the number, 0 otherwise
 
   box <- sample(1:(2*n),1)
   success = 0
@@ -78,8 +113,11 @@ strategy_three <- function(n, k, card){
 
 Pone <- function(n, k, strategy, nreps) {
 
-	# Estimates the probability of a single prisoner 
-	# succeeding in finding their number
+	# Estimates the probability of a single prisoner succeeding in finding their 
+  #number. The function runs the chosen strategy function nreps times to 
+  #obtain the probability of one prisoner succeeding given the chosen strategy,
+  #the number of times the prisoner is successful over the total number of 
+  #times the simulation is run, nreps. 
 
 	# Input:
 	# n - number of tries/max boxes prisoner can open
@@ -113,16 +151,21 @@ Pone <- function(n, k, strategy, nreps) {
 
 Pall <- function(n, strategy, nreps) {
 
-        # Estimates the probability of all prisoners
-        # succeeding in finding their number to be set free
+  # Estimates the probability of all prisoners succeeding in finding their 
+  #number to be set free. The function runs whichever strategy is chosen 2n 
+  #times to see if any prisoners have failed. If so, the entire group has 
+  #failed, only if all are successful then the group is successful. We repeat 
+  #this process nreps times to calculate the probability of success for all 
+  #prisoners, the number of times the chosen strategy ends in success for the 
+  #entire group divided by the total number of times the chosen strategy is run.
 
-        # Input:
-        # n - number of tries/max boxes prisoner can open
-        # strategy - 1, 2, or 3 to be implemented
-        # nreps - number of times to replicate Pone
+  # Input:
+  # n - number of tries/max boxes prisoner can open
+  # strategy - 1, 2, or 3 to be implemented
+  # nreps - number of times to replicate Pone
 
-        # Output:
-        # probability of all prisoners succeeding
+  # Output:
+  # probability of all prisoners succeeding
 
   cat("The probability of all prisoners succeeding, using strategy "
       , strategy, " is ")
